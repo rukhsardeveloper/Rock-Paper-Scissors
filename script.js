fe2098de49d7fa1c0771d6a023e7bb23ba290650
asp <asp@aptech.com>
2026-05-02 10:43:26 +0500

    added win and lose

--- script.js
+++ script.js
@@ -3,18 +3,50 @@
 let userscore=0
 let computerscore=0
 
-const choices=  document.getElementsByClassName('choice')
+const Choices=  document.getElementsByClassName('choice')
 // console.log(choices)
 
-for(let choice of choices){
+for(let choice of Choices){
     choice.addEventListener('click',function(){
     //  console.log("user click something------ ")
-    let userchoice= choice.getAttribute('id')
+    let userChoice= choice.getAttribute('id')
     // console.log(`user clicked: ${   userchoice}`)
-    playgame(userchoice)
+    playgame(userChoice)
     })
 }
-function playgame(userchoice){
-    console.log(`user clicked: ${userchoice}`)
+function playgame(userChoice){
+    console.log(`user clicked: ${userChoice}`)
+    const ComputerChoice=gencompchoice()//rock,paper,scissor
+    console.log(`Computer Chose:${ComputerChoice}`)
+    if(userChoice==ComputerChoice){
+        draw()
+    }else{
+        // console.log("should be a win or lose")
+        let userwin=true
+        //userchoice=rock,system have=paper or scissor
+        if(userChoice=="rock"){
+           userwin=ComputerChoice =="scissor"? true:false
+        }else if(userChoice=="paper"){
+           userwin=ComputerChoice =="rock"? true:false
+        
+    }else{
+        userwin=ComputerChoice =="paper"? true:false
+    }
+    showWinner(userwin)
+}}
+
+function gencompchoice(){
+    const options=['rock','paper','scissor']
+    const randomindex=Math.floor(Math.random()*3)//0,1,2
+    return options[randomindex]
 }
-   
+function draw(){
+    console.log("draw")
+}
+function showWinner(userwin){
+    if(userwin==true){
+        console.log("you win")
+    }else{
+       console.log("you lose") 
+    }
+}
